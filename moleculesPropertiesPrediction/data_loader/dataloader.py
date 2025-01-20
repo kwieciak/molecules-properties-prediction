f5c689bb966021be762d465f0da821b20183f461
kwieciak <bkwiecinski02@gmail.com>
2025-01-20 23:45:36 +0100

    added: dataloader

---
+++ moleculesPropertiesPrediction/data_loader/dataloader.py
@@ -0,0 +1,26 @@
+from torch_geometric.datasets import QM9
+from torch_geometric.loader import DataLoader
+
+
+def load_dataset(batch_size):
+    dataset_path = "./data"
+    qm9 = QM9(root=dataset_path)
+
+    # qm9 = qm9.shuffle()
+
+    # splitting the data
+    train_index = int(len(qm9.data.y) * 0.7)
+    val_index = train_index + int(len(qm9.data.y) * 0.1)
+    test_index = val_index + int(len(qm9.data.y) * 0.2)
+
+    # normalizing the data
+    data_mean = qm9.data.y[0:train_index].mean()
+    data_std = qm9.data.y[0:train_index].std()
+    qm9.data.y = (qm9.data.y - data_mean) / data_std
+
+    # putting datasets into dataloaders
+    train_loader = DataLoader(qm9[0:train_index], batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(qm9[train_index:val_index], batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(qm9[val_index:test_index], batch_size=batch_size, shuffle=True)
+
+    return train_loader, val_loader, test_loader
