c2e94af1e2f6e0a7f35eb5e1dcb7f9a180e2e9dc
kwieciak <bkwiecinski02@gmail.com>
2025-01-23 19:15:22 +0100

    added: gin model

--- moleculesPropertiesPrediction/main.py
+++ moleculesPropertiesPrediction/main.py
@@ -15,18 +15,19 @@ def main():
     modelGCN = GNN.GCN(64, 11)
     modelTransformCN = GNN.TransformerCN(11, 64)
     modelGATv2CN = GNN.Gatv2CN(11, 64)
+    modelGIN = GNN.GIN(11, 64)
 
     print()
     print('Dla batch = 24')
     start1 = time.time()
-    gcn_train_loss24, gcn_val_loss24, gcn_train_target24, gcn_train_y_target24 = trainer.train_epochs(epochs, modelGATv2CN, train_loader24, test_loader24, "GCN_model24.pt")
+    gcn_train_loss24, gcn_val_loss24, gcn_train_target24, gcn_train_y_target24 = trainer.train_epochs(epochs, modelGIN, train_loader24, test_loader24, "GCN_model24.pt")
     end1 = time.time()
     print('Time = ', end1 - start1)
 
     start2 = time.time()
     print()
     print('Dla batch = 32')
-    gcn_train_loss32, gcn_val_loss32, gcn_train_target32, gcn_train_y_target32 = trainer.train_epochs(epochs, modelGATv2CN,train_loader32, test_loader32, "GCN_model32.pt")
+    gcn_train_loss32, gcn_val_loss32, gcn_train_target32, gcn_train_y_target32 = trainer.train_epochs(epochs, modelGIN,train_loader32, test_loader32, "GCN_model32.pt")
     end2 = time.time()
     print('Time = ', end2 - start2)
 
--- moleculesPropertiesPrediction/model/GNN.py
+++ moleculesPropertiesPrediction/model/GNN.py
@@ -1,5 +1,6 @@
 import torch
-from torch_geometric.nn import GCNConv, global_mean_pool, TransformerConv, GATv2Conv
+from torch_geometric.nn import GCNConv, TransformerConv, GATv2Conv, GINConv,  global_mean_pool, global_add_pool
+from torch.nn import Sequential, Linear, ReLU, BatchNorm1d
 import torch.nn.functional as Fun
 
 
@@ -74,3 +75,56 @@ class Gatv2CN(torch.nn.Module):
         x = self.lin(x)
 
         return x
+
+
+class GIN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels=1):
+        super().__init__()
+        # Definicja warstw GINConv z silnie nieliniową funkcją
+        self.conv1 = GINConv(
+            Sequential(
+                Linear(in_channels, hidden_channels),
+                BatchNorm1d(hidden_channels),
+                ReLU(),
+                Linear(hidden_channels, hidden_channels),
+                ReLU()
+            )
+        )
+        self.conv2 = GINConv(
+            Sequential(
+                Linear(hidden_channels, hidden_channels),
+                BatchNorm1d(hidden_channels),
+                ReLU(),
+                Linear(hidden_channels, hidden_channels),
+                ReLU()
+            )
+        )
+        self.conv3 = GINConv(
+            Sequential(
+                Linear(hidden_channels, hidden_channels),
+                BatchNorm1d(hidden_channels),
+                ReLU(),
+                Linear(hidden_channels, hidden_channels),
+                ReLU()
+            )
+        )
+        self.lin1 = torch.nn.Linear(hidden_channels,hidden_channels)
+        self.lin2 = torch.nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        h = self.conv1(x, edge_index)
+        h = h.relu()
+        h = self.conv2(h, edge_index)
+        h = h.relu()
+        h = self.conv3(h, edge_index)
+
+        h = global_add_pool(h, batch)
+
+        h = self.lin1(h)
+        h = h.relu()
+        h = Fun.dropout(h, p=0.5, training=self.training)
+        h = self.lin2(h)
+
+        return h
