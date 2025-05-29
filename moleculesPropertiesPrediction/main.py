7bb283c630e6fdb641be73a37e277b0b72ad7768
kwieciak <bkwiecinski02@gmail.com>
2025-05-29 23:55:24 +0200

    changed: logic of the test() function, added the ability to select a specific task whose properties we want to predict

--- moleculesPropertiesPrediction/main.py
+++ moleculesPropertiesPrediction/main.py
@@ -4,6 +4,7 @@ from data_loader import dataloader
 from model import GNNwithMTL, trainer, tester, GNN
 import time
 
+
 warnings.filterwarnings("ignore")
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -11,19 +12,21 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def main():
     print(device)
-    epochs = 50
-    target_indices = [0, 3, 5, 10] #trenowac na od 0 do 17 a testoweac na 18 vs trenowany tylko na 18
-    task_weights = [1,2,3,4]
-    batch_size = 32
+    epochs = 100
+    target_indices = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18] #jakie taski sa wybierane do trenowania #trenowac na od 0 do 17 a testoweac na 18 vs trenowany tylko na 18
+    test_task = 0 #jaki task jest wybieramy do testu
+    task_indices = target_indices + ([test_task] if test_task not in target_indices else []) #sklejone listy aby w modelu utworzyc odpowiednia ilosc final_linear_layer
+    task_weights = None
+    batch_size = 24
     train_ratio = 0.7
     val_ratio = 0.1
     test_ratio = 0.2
-    dataset_usage_ratio = 0.001
+    dataset_usage_ratio = 0.05
 
     train_loader, val_loader, test_loader = dataloader.load_dataset(batch_size, train_ratio, val_ratio, test_ratio,
                                                                     target_indices, device, dataset_usage_ratio)
 
-    modelGIN = GNNwithMTL.GIN(11, 64, target_indices).to(device)
+    modelGIN = GNNwithMTL.GIN(11, 64, task_indices).to(device)
 
     print('Dla batch = 32')
     start = time.time()
@@ -38,6 +41,9 @@ def main():
                                                         device, task_weights)
     end = time.time()
     print('Time = ', end - start)
+
+    metrics = tester.test_gnn(test_loader, modelGIN, test_task, device)
+    print(f"Test RMSE: {metrics['rmse']:.4f}, MAE: {metrics['mae']:.4f}, R2: {metrics['r2']:.4f}")
 
     return
 
